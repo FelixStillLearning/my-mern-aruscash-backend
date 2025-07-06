c9213d309256a89fad871522991d027c115a6b67
FelixStillLearning <felixanggara077@gmail.com>
2025-07-06 23:41:09 +0700

    Initial commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/userController');
 
+// Test endpoint - tidak perlu database
+router.get('/test', (req, res) => {
+    res.json({ 
+        message: 'User routes working!', 
+        status: 'success',
+        timestamp: new Date().toISOString()
+    });
+});
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
